f533d08723d50773abf0dca9b5fe6a5202b8685e
Mona Lisa <j.bradley.hanrahan@gmail.com>
2021-02-02 13:56:30 -0600

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        private double balance = 0.0;
+        
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+            Console.WriteLine($"A deposit of ${amount.ToString("0.00")} has been added to your account. Thank you.");
+        }
+        public double GetBalance()
+        {
+            Console.WriteLine($"Your current account balance is ${balance.ToString("0.00")}.");
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount Acct1 = new BankAccount();
+            bool doMore = true;
+            do
+            {
+                Console.Write("For a deposit please enter the amount otherwise type \"balance\" for your current account balance. ");
+                var ans1 = Console.ReadLine();
+                ans1 = ans1.ToLower();
+                if (ans1 == "balance")
+                {
+                    Acct1.GetBalance();
+                }
+                else
+                {
+                    double dep = double.Parse(ans1);
+                    Acct1.Deposit(dep);
+
+                }
+                Console.Write("Would you like to make another transaction? ");
+                string answer = Console.ReadLine();
+                answer = answer.ToLower();
+                if (answer == "yes")
+                {
+                    doMore = true;
+                }
+                else
+                {
+                    doMore = false;
+                }
+            }
+            while (doMore == true);
+            Console.WriteLine("Thank you. Have a great day.");
+        }
+    }
+}
